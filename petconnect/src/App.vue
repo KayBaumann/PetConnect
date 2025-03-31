ebf8956b31ebadb8e0725fcd79a4ab2c5fa7a841
TIMON SOOM <timon_soom@sluz.ch>
2025-03-31 14:36:57 +0200

    added languages

--- petconnect/src/App.vue
+++ petconnect/src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <!-- Header oder Navigationsleiste -->
+
     <header>
       <nav>
         <ul>
           <li><router-link to="/">{{ $t('home') }}</router-link></li>
-          <li><router-link to="/about">{{ $t('about') }}</router-link></li>
           <li><router-link to="/search">{{ $t('search') }}</router-link></li>
+          <li><router-link to="/about">{{ $t('about') }}</router-link></li>
+          <li>
+            <button @click="toggleDropdown">{{ $t('language') }}</button>
+            <ul v-if="showDropdown" class="dropdown-menu">
+              <li id="dropdown" @click="changeLanguage('en')">English</li>
+              <li id="dropdown" @click="changeLanguage('de')">Deutsch</li>
+              <li id="dropdown" @click="changeLanguage('fr')">Français</li>
+              <li id="dropdown" @click="changeLanguage('it')">Italiano</li>
+            </ul>
+          </li>
         </ul>
       </nav>
     </header>
-
-    <!-- Router View für die dynamischen Komponenten -->
     <router-view />
   </div>
 </template>
@@ -19,13 +26,25 @@
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      showDropdown: false, 
+    };
+  },
+  methods: {
+    toggleDropdown() {
+      this.showDropdown = !this.showDropdown;
+    },
+    changeLanguage(language) {
+      this.$i18n.locale = language; 
+      this.showDropdown = false;
+    }
+  }
 };
 </script>
 
 <style scoped>
-/* Hier kannst du die grundlegenden Styles für deine App definieren */
 
-/* Einfache Grundstruktur für Header */
 header {
   background-color: #333;
   padding: 10px;
@@ -50,5 +69,27 @@ nav ul li a {
 
 nav ul li a:hover {
   text-decoration: underline;
+}
+
+.dropdown-menu {
+  position: absolute;
+  background-color: #f0f0f0;
+  border: 1px solid #ddd;
+  list-style: none;
+  padding: 0;
+  margin-top: 5px;
+  z-index: 10;
+}
+
+.dropdown-menu li {
+  padding: 10px;
+  cursor: pointer;
+}
+
+.dropdown-menu li:hover {
+  background-color: #ddd;
+}
+#dropdown {
+  color: black;
 }
 </style>
