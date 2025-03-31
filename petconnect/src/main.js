040a8a2f6e9c997bed77ba796ab66d7a2aea0d58
sarujan uthayachandran <sarujan_uthayachandran@sluz.ch>
2025-03-31 14:02:50 +0200

    reset whole project

--- petconnect/src/main.js
+++ petconnect/src/main.js
@@ -1,7 +1,5 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import i18n from './i18n';
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-const app = createApp(App);
-app.use(i18n);
-app.mount('#app');
+createApp(App).mount('#app')
